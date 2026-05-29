c421153c1c80669d3d4de773d64a6a7dbc749389
mathlenh <150336510+mathlenh@users.noreply.github.com>
2026-05-29 21:56:34 +0200

    Update config_cors.php

--- bakend/config_cors.php
+++ bakend/config_cors.php
@@ -1,10 +1,11 @@
 <?php
-
-header("Access-Control-Allow-Origin: http://localhost:3000"); 
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Credentials: true"); 
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=utf-8");
 
+// Gestion du preflight CORS (requête OPTIONS envoyée automatiquement par le navigateur)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
