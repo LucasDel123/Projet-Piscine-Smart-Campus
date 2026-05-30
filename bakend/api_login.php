3b23330f48e8729616d0cb92a8f6d9618f4405cb
LucasDel123 <lucas.delliste@hotmail.fr>
2026-05-30 12:38:39 +0200

    api_login.php

--- bakend/api_login.php
+++ bakend/api_login.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'bakend/config_cors.php';
-require_once '../../utils/response.php';
+require_once 'bakend/json_reponse.php';
 require_once '../../classes/Database.php';
 require_once '../../classes/Auth.php';
 
