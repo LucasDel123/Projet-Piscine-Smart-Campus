b5143b0d0b0bf435743e72df03e81ca9f92db1fc
LucasDel123 <lucas.delliste@hotmail.fr>
2026-05-30 11:56:13 +0200

    json_reponse.php

---
+++ bakend/json_reponse.php
@@ -0,0 +1,8 @@
+<?php
+
+function jsonResponse($data, $statusCode = 200) {
+    http_response_code($statusCode);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($data);
+    exit();
+}
