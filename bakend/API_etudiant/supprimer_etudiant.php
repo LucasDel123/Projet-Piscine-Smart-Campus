fc190f2b7b713059a6f763e0f8d42314f18dbffb
LucasDel123 <lucas.delliste@hotmail.fr>
2026-05-31 14:59:12 +0200

    supprimer_etudiant.php

---
+++ bakend/API_etudiant/supprimer_etudiant.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'bakend/config_cors.php';
+require_once 'bakend/json_reponse.php ';
+require_once 'bakend/config_database.php';
+require_once 'bakend/classes_authentification.php';
+
+Auth::checkRole('admin');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'DELETE') {
+    jsonResponse(["error" => "Méthode non autorisée"], 405);
+}
+
+if (!isset($_GET['id'])) {
+    jsonResponse(["error" => "ID de l'étudiant manquant. Utilisez ?id=X"], 400);
+}
+
+$db = Database::getConnection();
+
+$stmt = $db->prepare("DELETE FROM etudiant WHERE id_etudiant = ?");
+
+try {
+    $stmt->execute([$_GET['id']]);
+    jsonResponse(["message" => "Étudiant supprimé avec succès"], 200);
+} catch (PDOException $e) {
+    jsonResponse(["error" => "Impossible de supprimer cet étudiant (il a probablement des notes ou inscriptions liées)."], 409);
+}
